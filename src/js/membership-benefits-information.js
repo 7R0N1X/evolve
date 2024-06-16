c7f3e73263d3c1694b32518c15bef7ed6a17a67c
7R0N1X <techsoftec.tronix@gmail.com>
2024-06-16 13:02:11 -0500

    se agrega sección de beneficios

---
+++ src/js/membership-benefits-information.js
@@ -0,0 +1,19 @@
+import img1 from "../../public/member benefits/1.avif"
+import img2 from "../../public/member benefits/2.avif"
+
+export const information = [
+  {
+    imagen: img1,
+    imgPosition: "lg:justify-start",
+    descriptionPosition: "lg:right-0",
+    title: "Member focused",
+    description: "At EVOLVE we are dedicated to putting our members first by tailoring our services to meet their individual needs. From personalised training programs to attentive staff members ready to assist at all times, we strive to create a welcoming and supportive environment where members feel valued and motivated to achieve their fitness goals."
+  },
+  {
+    imagen: img2,
+    imgPosition: "lg:justify-end",
+    descriptionPosition: "lg:left-0",
+    title: "Tailored programming",
+    description: "Let's work together to create a comprehensive plan that seamlessly accommodates your family commitments, manages and heals from injuries, and overcomes career obstacles. Regardless of life's challenges, we will navigate a path towards your goals, making the journey as smooth as possible despite the twists and turns along the way."
+  }
+]
