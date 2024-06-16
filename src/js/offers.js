0ae3188e95e5967f15fad440ee259bd0eed3a38d
7R0N1X <techsoftec.tronix@gmail.com>
2024-06-16 15:51:16 -0500

    feat(index): agrega sección what we offer

---
+++ src/js/offers.js
@@ -0,0 +1,27 @@
+import img1 from "../../public/offer/1.avif"
+import img2 from "../../public/offer/2.avif"
+import img3 from "../../public/offer/3.avif"
+import img4 from "../../public/offer/4.avif"
+
+export const OFFERS = [
+  {
+    img: img1,
+    title: "The gym",
+    description: "Everything from traditional iron plates to modern Nautilus equipment perfectly suited to any athlete."
+  },
+  {
+    img: img2,
+    title: "Cardio",
+    description: "The latest range of cardiovascular equipment, Ideal for your cardio routine or as a pre-training warm-up!"
+  },
+  {
+    img: img3,
+    title: "Classes",
+    description: "We offer a wide range of classes, boot camps and one to one personal training sessions."
+  },
+  {
+    img: img4,
+    title: "Cafe",
+    description: "We provide house-made barista-style coffees, protein shakes, pre-workouts, intra-workouts and more."
+  }
+]
