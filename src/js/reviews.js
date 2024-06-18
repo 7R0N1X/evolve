25b75def150f0740d081aeb427938cf8890f01bb
7R0N1X <techsoftec.tronix@gmail.com>
2024-06-18 13:31:34 -0500

    feat(components): agrega sección de reseñas

---
+++ src/js/reviews.js
@@ -0,0 +1,27 @@
+import img4 from "../../public/review/4.avif";
+import img5 from "../../public/review/5.avif";
+import img6 from "../../public/review/6.avif";
+
+export const REVIEWS = [
+  {
+    rating: 5,
+    reviewText: "I absolutely love this gym! Top-notch facilities, friendly staff, fantastic classes. Perfect for workouts and staying motivated. Highly recommend!",
+    img: img4,
+    reviewerName: "Jason King",
+    reviewerTitle: "Sponsored Athlete"
+  },
+  {
+    rating: 5,
+    reviewText: "Great atmosphere, variety of equipment. Knowledgeable trainers push you to your best. Definitely worth a visit for anyone serious about fitness!",
+    img: img5,
+    reviewerName: "Alex Smith",
+    reviewerTitle: "Fitness Enthusiast"
+  },
+  {
+    rating: 5,
+    reviewText: "Amazing place! Supportive community, challenging workouts, real results seen. Love it here and would recommend to anyone looking for quality gym!",
+    img: img6,
+    reviewerName: "Samantha Lee",
+    reviewerTitle: "Gym Regular"
+  }
+]
