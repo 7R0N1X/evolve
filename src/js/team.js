cc5829ba4cb966929773992c5a339a941eeabf96
7R0N1X <techsoftec.tronix@gmail.com>
2024-06-19 10:54:49 -0500

    feat(components): agrega sección de team

---
+++ src/js/team.js
@@ -0,0 +1,44 @@
+import img1 from "../../public/team/1.png"
+import img2 from "../../public/team/2.png"
+import img3 from "../../public/team/3.png"
+import img4 from "../../public/team/4.png"
+import img5 from "../../public/team/5.png"
+import img6 from "../../public/team/6.png"
+import img7 from "../../public/team/7.png"
+import img8 from "../../public/team/8.png"
+
+export const TEAM = [
+  {
+    imagePath: img1,
+    name: "First Last",
+    title: "Owner & Trainer",
+  }, {
+    imagePath: img2,
+    name: "First Last",
+    title: "Owner & Trainer",
+  }, {
+    imagePath: img3,
+    name: "First Last",
+    title: "Owner & Trainer",
+  }, {
+    imagePath: img4,
+    name: "First Last",
+    title: "Owner & Trainer",
+  }, {
+    imagePath: img5,
+    name: "First Last",
+    title: "Owner & Trainer",
+  }, {
+    imagePath: img6,
+    name: "First Last",
+    title: "Owner & Trainer",
+  }, {
+    imagePath: img7,
+    name: "First Last",
+    title: "Owner & Trainer",
+  }, {
+    imagePath: img8,
+    name: "First Last",
+    title: "Owner & Trainer",
+  },
+]
