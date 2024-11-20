b87bff98240a9f9ac83d06c71a87699ea7958841
7R0N1X <techsoftec.tronix@gmail.com>
2024-11-20 17:36:29 -0500

    feat: change animations
    The global animation is changed to two animations, one for the image and one for the description.

--- src/js/membership-benefits-information.js
+++ src/js/membership-benefits-information.js
@@ -13,8 +13,9 @@ export const information = [
     descriptionPosition: "lg:right-0",
     title: "Member focused",
     description: "At EVOLVE we are dedicated to putting our members first by tailoring our services to meet their individual needs. From personalised training programs to attentive staff members ready to assist at all times, we strive to create a welcoming and supportive environment where members feel valued and motivated to achieve their fitness goals.",
-    data_aos: "fade-right",
-    data_aos_delay:"200"
+    data_aos_img: "fade-right",
+    data_aos_desc: "fade-left",
+    data_aos_delay: "200"
   },
   {
     imagen: [{
@@ -25,7 +26,8 @@ export const information = [
     descriptionPosition: "lg:left-0",
     title: "Tailored programming",
     description: "Let's work together to create a comprehensive plan that seamlessly accommodates your family commitments, manages and heals from injuries, and overcomes career obstacles. Regardless of life's challenges, we will navigate a path towards your goals, making the journey as smooth as possible despite the twists and turns along the way.",
-    data_aos:"fade-left",
-    data_aos_delay:"200"
+    data_aos_img: "fade-left",
+    data_aos_desc: "fade-right",
+    data_aos_delay: "200"
   }
 ]
